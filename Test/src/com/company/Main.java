b90880bd4029f34e90f076cfeed045f5c755a783
arjunbalussery <arjunac009@gmail.com>
2018-05-03 11:17:00 +0530

    modification in main

--- Test/src/com/company/Main.java
+++ Test/src/com/company/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         String [] abc = StringGenerator.generator(2,"abcde");
 
-
        Arrays.sort(abc);
         //System.out.println(abc[10]);
         System.out.println(BinarySearch.binarySearch(abc,abc[0],0,abc.length));
